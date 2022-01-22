02da6343f23504367798b17d1beb66fea9314b69
Yildiz Aydogan <94450275+YildizAydogan@users.noreply.github.com>
2022-01-22 19:59:46 +0100

    jUnitTest

--- src/main/java/org/junit/C03_ArrayEsitMi.java
+++ src/main/java/org/junit/C03_ArrayEsitMi.java
@@ -5,7 +5,11 @@ import java.util.Arrays;
 public class C03_ArrayEsitMi {
  /*
 Task : iki array'i esitliğini kiyaslayan method creat ediniz
-   */
+  */
+
+ public static boolean arrayKiyasla(Object[] a, Object[] b) {
+  Arrays.sort(a);
+  Arrays.sort(b);
+  return Arrays.equals(a, b);
  }
-
-
+}
